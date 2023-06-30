b99ac9fdbbc78fd0200308666e441f89766a7b63
keFEAR1989 <129889358+keFEAR1989@users.noreply.github.com>
2023-06-30 17:33:07 +0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 import telebot
 from extensions import APIException, CryptoConverter
-
-TOKEN = '6071252171:AAEMLuZ0tAy_vA4yASgWZDsBu1yxz58Airo'
+from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
